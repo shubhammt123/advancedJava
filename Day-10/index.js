45e01037ebfacaa7e660bab365a6b20100d57fad
shubhammt123 <shubhammt123@gmail.com>
2025-09-18 21:05:57 +0530

    Js Introduction

---
+++ Day-10/index.js
@@ -0,0 +1,9 @@
+// console.log("Hello world");
+
+// alert("Hello Everyone, Good evening");
+
+// console.log(confirm("Are you want to leave this page?"));
+
+// console.log(prompt("Enter first number"));
+
+// variables , datatypes , operators , control flows
---
+++ Day-10/script.js
@@ -0,0 +1,1 @@
+console.log("execution from Script file")
